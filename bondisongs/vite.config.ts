280e391312904004a9b6a83b12f05db8b811698f
Javierpaez87 <paez.javierandres@gmail.com>
2026-04-28 12:22:53 -0300

    Updated vite.config.ts

--- bondisongs/vite.config.ts
+++ bondisongs/vite.config.ts
@@ -6,12 +6,4 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  define: {
-    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(
-      process.env.VITE_SUPABASE_URL ?? 'https://twrwhcetxqpokuxfmicm.supabase.co'
-    ),
-    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(
-      process.env.VITE_SUPABASE_ANON_KEY ?? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3cndoY2V0eHFwb2t1eGZpbWNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyMzYzODAsImV4cCI6MjA5MjgxMjM4MH0.l-jNagjY968DjECaGdr7ZnDfT2MovLYBVBXvo8v0V_A'
-    ),
-  },
 });
